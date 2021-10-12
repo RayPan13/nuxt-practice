1b19e3650660a5d4abef737282e9133fd6689c43
RayPan <Ray0928066818@gmail.com>
2021-10-12 17:22:56 +0800

    完成 fratured 區塊 tab 切換互動效果

--- components/LayoutFeatured.vue
+++ components/LayoutFeatured.vue
@@ -3,7 +3,16 @@
         <div class="container">
             <h1>Featured Product</h1>
             <div class="tags">
-                <div class="tag"></div>
+                <ul>
+                    <li
+                        v-for="(obj, index) of tabs"
+                        :key="obj.id"
+                        :class="{ active: tabActive[index] }"
+                        @click="clickTab(index)"
+                    >
+                        {{ obj.tab }}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -14,12 +23,13 @@ export default {
     name: 'LayoutFeatured',
     data() {
         return {
-            tags: [
-                { id: 1, tag: 'Popular' },
-                { id: 2, tag: 'Creams' },
-                { id: 3, tag: 'Chocolates' },
-                { id: 4, tag: 'Fruit' },
-                { id: 5, tag: 'Dulce de leche' },
+            tabActive: [1, 0, 0, 0, 0],
+            tabs: [
+                { id: 1, tab: 'Popular' },
+                { id: 2, tab: 'Creams' },
+                { id: 3, tab: 'Chocolates' },
+                { id: 4, tab: 'Fruit' },
+                { id: 5, tab: 'Dulce de leche' },
             ],
             product: [
                 {
@@ -73,6 +83,17 @@ export default {
             ],
         }
     },
+    mounted() {
+        for (let i = 0; i < this.tabs.length; i++) {
+            this.tabs[i].id = i + 1
+        }
+    },
+    methods: {
+        clickTab(idx) {
+            this.tabActive = [0, 0, 0, 0, 0]
+            this.tabActive[idx] = 1
+        },
+    },
 }
 </script>
 <style lang="scss" scoped>
@@ -82,7 +103,7 @@ export default {
     margin-bottom: 24px;
     h1 {
         text-align: center;
-        margin: 0;
+        margin: 12px 0;
         font-size: 3rem;
         padding-bottom: 12px;
         &::after {
@@ -92,6 +113,27 @@ export default {
             height: 4px;
             background-color: map-get($color, main);
             margin: 8px auto 0;
+        }
+    }
+}
+.tags {
+    ul {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+    li {
+        display: inline-block;
+        font-size: 1.6rem;
+        font-weight: 300;
+        margin: 0 12px 8px;
+        padding-bottom: 4px;
+        cursor: pointer;
+        &:hover {
+            color: map-get($color, main);
+        }
+        &.active {
+            border-bottom: 2px solid map-get($color, main);
         }
     }
 }
