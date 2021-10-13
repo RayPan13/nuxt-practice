7c464d02ae95b17e0cf7d8e53fb7c15be0d5429c
RayPan <Ray0928066818@gmail.com>
2021-10-13 11:43:03 +0800

    完成 featured 產品區塊畫面

--- components/LayoutFeatured.vue
+++ components/LayoutFeatured.vue
@@ -2,7 +2,7 @@
     <div class="featured">
         <div class="container">
             <h1>Featured Product</h1>
-            <div class="tags">
+            <div class="tabs">
                 <ul>
                     <li
                         v-for="(obj, index) of tabs"
@@ -11,6 +11,26 @@
                         @click="clickTab(index)"
                     >
                         {{ obj.tab }}
+                    </li>
+                </ul>
+            </div>
+            <div class="product">
+                <ul>
+                    <li v-for="obj of product" :key="obj.id">
+                        <div class="box">
+                            <div class="pic">
+                                <a href="javascript:;">{{ obj.title }}</a>
+                                <img :src="obj.src" alt="" />
+                            </div>
+                            <div class="txt">
+                                <h3>{{ obj.title }}</h3>
+                                <p class="price">{{ obj.price }}</p>
+                            </div>
+                            <button type="button">
+                                <fa :icon="['fas', 'shopping-cart']" />
+                                <span>ADD TO CART</span>
+                            </button>
+                        </div>
                     </li>
                 </ul>
             </div>
@@ -25,11 +45,11 @@ export default {
         return {
             tabActive: [1, 0, 0, 0, 0],
             tabs: [
-                { id: 1, tab: 'Popular' },
-                { id: 2, tab: 'Creams' },
-                { id: 3, tab: 'Chocolates' },
-                { id: 4, tab: 'Fruit' },
-                { id: 5, tab: 'Dulce de leche' },
+                { tab: 'Popular' },
+                { tab: 'Creams' },
+                { tab: 'Chocolates' },
+                { tab: 'Fruit' },
+                { tab: 'Dulce de leche' },
             ],
             product: [
                 {
@@ -87,6 +107,9 @@ export default {
         for (let i = 0; i < this.tabs.length; i++) {
             this.tabs[i].id = i + 1
         }
+        for (let i = 0; i < this.product.length; i++) {
+            this.product[i].id = i + 1
+        }
     },
     methods: {
         clickTab(idx) {
@@ -116,7 +139,8 @@ export default {
         }
     }
 }
-.tags {
+.tabs {
+    margin-bottom: 12px;
     ul {
         margin: 0;
         padding: 0;
@@ -127,13 +151,96 @@ export default {
         font-size: 1.6rem;
         font-weight: 300;
         margin: 0 12px 8px;
-        padding-bottom: 4px;
+        line-height: 1.5;
+        user-select: none;
         cursor: pointer;
         &:hover {
             color: map-get($color, main);
         }
         &.active {
             border-bottom: 2px solid map-get($color, main);
+        }
+    }
+}
+.product {
+    width: 100%;
+    ul {
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        font-size: 0;
+    }
+    li {
+        list-style: none;
+        flex-basis: 25%;
+        padding: 0 12px;
+        margin-bottom: 24px;
+        box-sizing: border-box;
+        @include media(1024) {
+            flex-basis: 33.33%;
+        }
+        @include media(640) {
+            flex-basis: 50%;
+        }
+        @include media(400) {
+            flex-basis: 100%;
+        }
+        .box {
+            border: 1px solid #d9d9d9;
+            border-radius: 4px;
+            padding-bottom: 12px;
+            &:hover {
+                border-color: map-get($color, main);
+            }
+        }
+        .txt {
+            padding: 0 5%;
+        }
+        .pic {
+            margin-bottom: 8px;
+            position: relative;
+            a {
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+            }
+            img {
+                width: 100%;
+            }
+        }
+        h3 {
+            margin: 0 0 8px;
+            font-size: 1.8rem;
+            font-weight: 500;
+        }
+        p {
+            margin: 0 0 8px;
+            font-size: 1.6rem;
+            &::before {
+                content: '$';
+                display: inline-block;
+                padding-right: 4px;
+            }
+        }
+        button {
+            display: block;
+            border: 0;
+            border-radius: 4px;
+            background-color: map-get($color, main);
+            color: #fff;
+            text-align: center;
+            width: 90%;
+            padding: 12px 0;
+            margin: 0 auto;
+            cursor: pointer;
+            span {
+                font-size: 1.4rem;
+                font-weight: 600;
+            }
         }
     }
 }
