4455eb3f0cca1d694354e82b8681bbc73893772c
RayPan <Ray0928066818@gmail.com>
2021-10-20 16:04:29 +0800

    新增 FAVORITES 頁面

---
+++ pages/favorites.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="favorites">
+        <h1>favorites</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        window.addEventListener('scroll', this.scrollhandler, true)
+        this.closeNav()
+    },
+    methods: {
+        scrollhandler() {
+            this.scrollTop = document.documentElement.scrollTop
+        },
+        closeNav() {
+            this.$store.commit('toggleNav', false)
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
