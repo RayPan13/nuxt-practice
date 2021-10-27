eb527f12d3465d006023705805f8923bb87e85bb
RayPan <Ray0928066818@gmail.com>
2021-10-27 11:54:02 +0800

    修改 generate 設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
     ...routerBase,
-    // generate: {
-    //     subFolders: false,
-    // },
+    generate: {
+        subFolders: false,
+    },
 }
