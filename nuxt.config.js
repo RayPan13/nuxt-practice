1501992de1b7f230565e58eb0f87c8c9d59c32e6
RayPan <Ray0928066818@gmail.com>
2021-10-27 11:05:01 +0800

    微調設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,4 +75,8 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
+    router: { base: '/<repository-name>/' },
+    generate: {
+        subFolders: false,
+    },
 }
