7fcd93e73326a28af0260d5c9e5132eff69a517e
RayPan <Ray0928066818@gmail.com>
2021-10-27 11:42:03 +0800

    調整 router 設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,11 @@
+const routerBase =
+    process.env.DEPLOY_ENV === 'GH_PAGES'
+        ? {
+              router: {
+                  base: '/<repository-name>/',
+              },
+          }
+        : {}
 export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
@@ -75,7 +83,8 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
-    generate: {
-        subFolders: false,
-    },
+    ...routerBase,
+    // generate: {
+    //     subFolders: false,
+    // },
 }
